dfd76c77551eef5bed9afb3fd01dae32b7e38365
Ithan lara <ithan.lara.f@gmail.com>
2018-10-16 17:37:13 +0200

    Update functions to e6

--- Arsy.js
+++ Arsy.js
@@ -8,23 +8,19 @@
     // Return a random value from and array
     // --------------------------------------
 
-    Array.prototype.random = function(){                            // Random function definition
-        return this[Math.floor(Math.random() * this.length)]            // Return a value from itself
-    }                                                               // End of function random
+    Array.prototype.random = function(){
+        return this[Math.floor(Math.random() * this.length)]
+    }
 
     // --------------------------------------
     // Shufle the given array
     // --------------------------------------
 
 
-    Array.prototype.shufle = function (){                                                                   // Shufle function definition
-        let arrayToShufle = JSON.parse(JSON.stringify(this)),                                                   // Save the current Array
-            arrayShufled  = [  ];                                                                               // Create a new Empty Array to be returned
-        while(arrayToShufle.length > 0){                                                                        // While arrayToShuffle have more then 1 Element select a random one
-            arrayShufled.push(arrayToShufle.splice(Math.floor(Math.random() * arrayToShufle.length),1)[0])          // Push into arrayShufled the selected Element and remove it from arrayToShuffle
-        }                                                                                                       // End of while
-        return arrayShufled;                                                                                    // Return the array arrayShufled shufled
-    }                                                                                                       // End of function shufle
+    Array.prototype.shufle = function (){
+      return this.sort( _ => Math.random() - 0.5);
+    }
+
 
     // --------------------------------------
     //Return the array switching 2 values
@@ -52,70 +48,46 @@
     //Return a window from the array
     // --------------------------------------
 
-    Array.prototype.window = function(startingIndex , lengthOfTheWindow){               // Window function definition
-        let arrayWindow = [];                                                               // Create the array to return
-        arrayWindow = this.filter((val,index)=>{                                            // Set arrayWindow to the filter results
+    Array.prototype.window = function(startingIndex , lengthOfTheWindow){                       // Window function definition
+        let arrayWindow = [];                                                                   // Create the array to return
+        arrayWindow = this.filter((val,index)=>{                                                // Set arrayWindow to the filter results
             return index >= startingIndex && index < startingIndex + lengthOfTheWindow          // Return the elements where the index is higher then the startingIndex and less then the lengthOfTheWindow +1
-        })                                                                                  // End of filter
-        return arrayWindow                                                                  // Return the results
-    }                                                                                   // End of function window
+        })                                                                                      // End of filter
+        return arrayWindow                                                                      // Return the results
+    }                                                                                           // End of function window
     // ------------------------------------------------
     //Return and array of index of the given element
     // ------------------------------------------------
 
-    Array.prototype.search = function(element){                             // Search function definition
-        let arrayElementIndex = [];                                             // Create the array to return
-        this.map( (val,index)=>{                                            // Loop thrue the array
+    Array.prototype.search = function(element){                                     // Search function definition
+        let arrayElementIndex = [];                                                 // Create the array to return
+        this.map( (val,index)=>{                                                    // Loop thrue the array
             if(element === val){ arrayElementIndex.push(index) }                    // If the element match the value of the array, return the index of the value
-        });                                                                     // End of loop
-        if(arrayElementIndex.length > 0){                                       // If the array length is higher then 0
+        });                                                                         // End of loop
+        if(arrayElementIndex.length > 0){                                           // If the array length is higher then 0
             return arrayElementIndex;                                               // Return de array
-        } else {                                                                // If its 0
+        } else {                                                                    // If its 0
             return false                                                            // Return false
-        }                                                                       // End of if
-    }                                                                       // End of function search
+        }                                                                           // End of if
+    }                                                                               // End of function search
 
     // --------------------------------------
     //Remove an element from the array
     // --------------------------------------
 
-    Array.prototype.remove = function(element, boleean){                                // Remove function definition
-        let arrayRemovedElements = JSON.parse(JSON.stringify(this));                       // Create a new array to return from self
-        boleean = boleean || false;                                                        // If no boleean is given, set it to false
-        if(this.indexOf(element) > -1){                                                    // If the given element exist do
-            if(boleean){                                                                        // If the given boleean is true process all the elements
-                while(arrayRemovedElements.indexOf(element) > -1){                                   // While there is a Match
-                    arrayRemovedElements.splice(arrayRemovedElements.indexOf(element),1);                // Delete the element matched
-                }                                                                                        // End of while
-            } else {                                                                            // If the boleean is false
-                arrayRemovedElements.splice(arrayRemovedElements.indexOf(element),1);                // Delete 1 element
-            }                                                                                   // End of If
-            return arrayRemovedElements;                                                        // Return arrayRemovedElements
-        } else {                                                                                // If the given element dosn't exist
-            return false                                                                             // Return false
-        }                                                                                       // End of if
-    }                                                                                   // End of function remove
-
-    // --------------------------------------
-    //Return the next value
-    // --------------------------------------
-
-   Array.prototype.next = function(currentValue){                                               // Next function definition
-        if(this.indexOf(currentValue) > -1 && this.indexOf(currentValue) < this.length - 1){        // If the value exist and is less then the length of the array
-            return this[this.indexOf(currentValue) + 1]                                                 // return the next value
-        } else {                                                                                    // If not
-            return false;                                                                               // Return false
+    Array.prototype.remove = function(element, boleean){                                            // Remove function definition
+        let arrayRemovedElements = JSON.parse(JSON.stringify(this));                                // Create a new array to return from self
+        boleean = boleean || false;                                                                 // If no boleean is given, set it to false
+        if(this.indexOf(element) > -1){                                                             // If the given element exist do
+            if(boleean){                                                                            // If the given boleean is true process all the elements
+                while(arrayRemovedElements.indexOf(element) > -1){                                  // While there is a Match
+                    arrayRemovedElements.splice(arrayRemovedElements.indexOf(element),1);           // Delete the element matched
+                }                                                                                   // End of while
+            } else {                                                                                // If the boleean is false
+                arrayRemovedElements.splice(arrayRemovedElements.indexOf(element),1);               // Delete 1 element
+            }                                                                                       // End of If
+            return arrayRemovedElements;                                                            // Return arrayRemovedElements
+        } else {                                                                                    // If the given element dosn't exist
+            return false                                                                            // Return false
         }                                                                                           // End of if
-   }                                                                                            // End of function next
-
-   // --------------------------------------
-   //Return the previus value
-   // --------------------------------------
-
-   Array.prototype.prev = function(currentValue){                                               // prev function definition
-        if(this.indexOf(currentValue) > -1 && this.indexOf(currentValue) > 0){                       // If the value exist and is less then the length of the array
-            return this[this.indexOf(currentValue) - 1]                                                 // return the prev value
-        } else {                                                                                    // If not
-            return false;                                                                               // Return false
-        }                                                                                           // End of if
-    }                                                                                          // End of function prev
+    }                                                                                               // End of function remove
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Arsy.js
-Small library to add additional functionality to arrays 
+Small library to add additional functionality to arrays
 
 You have 8 (for now ) buildin extra functions to Arrays
 ```
@@ -14,69 +14,51 @@ You have 8 (for now ) buildin extra functions to Arrays
 ```
 
 ### Any of this function will modify the array itself, they only will return the expected value
- 
+
  ___________________________________________________________
 ## Array.random()
 
   This will return a random value from Array
 
   ``  [1,2,3].random() `` will return ``1`` or ``2`` or ``3``
-  
+
   ___________________________________________________________
   ## Array.shufle()
 
   This will shufle the current Array
 
   ``  [1,2,3].shufle() `` will return ``[2,3,1]`` or ``[3,1,2]`` ...
-  
+
   ___________________________________________________________
   ## Array.switch(first element index to switch, second element index to switch)
 
   This will swap 2 values from the given index in the array
-      
+
   ``  [1,2,3].switch(1,2) `` will return `` [1,3,2]``
-  
-  
+
+
   ___________________________________________________________
   ## Array.window(Starting index, size)
 
   This will return a window from the index to the size
 
   ``  [1,2,3,4,5,6].window(2,3) `` will return ` [3,4,5]``
-  
+
   ___________________________________________________________
   ## Array.search(value to search)
 
   This will return the index of all the matched elements,if it find any match, always will return an array, if it can't find any match, will return false
   ``  [1,2,3,4,4].search(4) `` will return ``[3,4]``
-  
+
   ``  [1,2,3,4,4].search(5) `` will return ``false``
-  
+
    ___________________________________________________________
   ## Array.remove(element to remove, will it remove all the matches, or only the first one? (true or false expected))
 
   This will remove the first matched item, if true is given as second parameter, it will remove all the matched items, if no matches are found, it will return false
-  
+
   ``  [1,2,3,4,4].remove(4) `` will return ``[1,2,3,4]``
-  
+
   ``  [1,2,3,4,4].remove(4,true) `` will return ``[1,2,3]``
-  
+
   ``  [1,2,3,4,4].remove(5,true) `` will return ``false``
-  
-    ___________________________________________________________
-  ## Array.next(value)
-
-  Return the next element in the array from the first match of the given element
-
-  ``  [1,2,3,4,5,6].next(2) `` will return `` 3``
-  
-  ___________________________________________________________
-  ## Array.prev(value)
-
-  Return the previus element in the array from the first match of the given element
-
-  ``  [1,2,3,4,5,6].prev(2) `` will return `` 1``
-  
-  ___________________________________________________________
-  
- 
